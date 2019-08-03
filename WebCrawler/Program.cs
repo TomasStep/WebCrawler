ab4d8639025e6df6ca7b134d914336d3f0d0e47c
Tomas Stepanavicius <ttomass@gmail.com>
2019-08-03 20:16:42 +0300

    Added (Microsoft.Net.Http and HtmlAgilityPack)
    Created project and added some Nuget Packages

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartCrawlerAsync();
+
+            Console.Read();
+        }
+
+        private static async Task StartCrawlerAsync()
+        {
+            var url = "https://www.norwegian.com/en/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument(); // to understand html content correctly
+            htmlDocument.LoadHtml(html);
+        }
+    }
+}
