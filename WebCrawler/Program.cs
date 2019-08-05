5741a344398d6bb42a689e2ef7fa4307a88c5c39
Tomas Stepanavicius <ttomass@gmail.com>
2019-08-05 18:19:50 +0300

    The correct url (based on HTTP Analyzer)
    Changed corrupted URL

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -12,11 +12,15 @@ namespace WebCrawler
             StartCrawlerAsync();
 
             Console.Read();
+            Console.ReadLine();
         }
 
         private static async Task StartCrawlerAsync()
         {
             var url = "https://www.norwegian.com/en/";
+            var urlMain = "https://www.norwegian.com/en/";
+            var url =
+                "https://www.norwegian.com/en/start/booking/farecalendar/?A_City=RIX&AdultCount=1&ChildCount=0&CurrencyCode=EUR&D_City=OSL&D_Day=01&D_Month=201909&D_SelectedDay=01&IncludeTransit=true&InfantCount=0&R_Day=05&R_Month=201908&TripType=1";
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(url);
 
