dab9a990607320a9d14886cd99391fd94226ddbf
Tomas Stepanavicius <ttomass@gmail.com>
2019-08-05 18:21:52 +0300

    Temp commit
    TEMP

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -11,21 +12,29 @@ namespace WebCrawler
         {
             StartCrawlerAsync();
 
-            Console.Read();
             Console.ReadLine();
         }
 
         private static async Task StartCrawlerAsync()
         {
-            var url = "https://www.norwegian.com/en/";
             var urlMain = "https://www.norwegian.com/en/";
             var url =
                 "https://www.norwegian.com/en/start/booking/farecalendar/?A_City=RIX&AdultCount=1&ChildCount=0&CurrencyCode=EUR&D_City=OSL&D_Day=01&D_Month=201909&D_SelectedDay=01&IncludeTransit=true&InfantCount=0&R_Day=05&R_Month=201908&TripType=1";
+
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(url);
 
-            var htmlDocument = new HtmlDocument(); // to understand html content correctly
+            Console.WriteLine(html);
+            var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
+
+            var table = htmlDocument.DocumentNode.Descendants("table").Where(node =>
+                node.GetAttributeValue("class", "").Equals("lowfare-calendar__table")).ToList();
+
+//            foreach (var t in table)
+//            {
+//                Console.WriteLine(t.Descendants("tr").Where(node => node.GetAttributeValue("class","").Equals("lowfare-calendar__row lowfare-calendar__row--animate")));    
+//            }
         }
     }
 }
